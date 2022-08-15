a70a2837db00bce64343b12e9e98817496c4bae3
Coline <coline.ducourtieux@gmail.com>
2022-08-15 12:00:41 +0200

    exo arrays

---
+++ RevisionsPHP/PHP_ARRAYS/ExoBoucle1.php
@@ -0,0 +1,7 @@
+<?php
+
+$message = "Bonjour, je suis dans une boucle!";
+
+for ($i=1; $i<=10; $i++){
+    print("\n".$message);
+}
---
+++ RevisionsPHP/PHP_ARRAYS/ExoBoucle4-5-6.php
@@ -0,0 +1,16 @@
+<?php
+
+$val=[20,50,7,54];
+
+$val[]=150;
+$val[]=300;
+
+foreach ($val as $valeur)
+{
+    print($valeur."\n");
+}
+
+for ($i=0; $i<=count($val); $i++)
+{
+    print($valeur);
+}
